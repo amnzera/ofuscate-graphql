29b983e417f5a415fcad53657fb8f30af5a1a0dc
Alexandre Mendes nunes <amn_scout@hotmail.com>
2023-02-27 09:47:43 -0300

    commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Installation
+> `npm i ofuscate-graphql --save-dev`
+
+# How use this package
+
+* First Step: Add 'keyOfuscate' in file environment.
+* Example:
+
+> `export const environment = {
+keyOfuscate: "118ed793f750d7b8212874e99f8495f3"
+};
+`
+
+
+* Second step: Your file must end with the .query.ts extension
+
+* Example:
+
+> `your-file.query.ts`
+
+* And your querys must be "const export":
+
+
+> `export const Your_Query = gql`
+query YourQuery {\
+data {  \
+subData\
+}\
+}
+`;`
+
+* Third step: Run the command before the final build of the project
+
+* Example:
+
+> `node node_modules/ofuscate-graphql/index.mjs`
+
+* You can also add this command in the 'script' of 'package.json'
+
+> `"build": "node node_modules/ofuscate-graphql/index.mjs && ng build"`
+
+* After being encrypted by 'CryptoJS' you can decrypt it before making the request
+
+* Example:
+
+> `import * as CriptoJS from 'crypto-js`
+
+> `import { gql } from "graphql-tag";`
+
+> `const decrypted = CryptoJS.AES.decrypt(My_query, environment.keyOfuscate);`
+
+> `const decrypted = CryptoJS.AES.decrypt(My_query, environment.keyOfuscate);`
+
+> `const originalQuery = decrypted.toString(CryptoJS.enc.Utf8);`
+
+> `return gql(decryptedQuery);`
+
+
+
+
+* ATTENTION step remember this command will rewrite your graphql-tag
+
+
+
+### Additional Details
+ * Dependencies: CryptoJS(https://github.com/brix/crypto-js)
+
+# Credits
+These definitions were written by [Alexandre_Nunes](https://github.com/amnzera).
---
+++ index.mjs
@@ -0,0 +1,91 @@
+import fs from 'fs';
+import path from 'path';
+import CryptoJS from 'crypto-js';
+
+
+const dirPathEnv = './src/environments';
+const environmentTs = getEnvironment(dirPathEnv);
+const secretKey = readEnvironment(environmentTs);
+
+if(secretKey){
+    const dirPath = './src/app/';
+    const filesWithQueryTs = getFilesWithQueryTs(dirPath);
+    filesWithQueryTs.forEach(filepath => {
+        encryptQuery(filepath,secretKey);
+    });
+} else {
+    console.log("-------------------------------- Please specify a 'keyOfuscate' in the environment file --------------------------------");
+}
+
+
+export function getEnvironment(dirPath) {
+    const filesTS = []
+    const fileList = fs.readdirSync(dirPath);
+    fileList.forEach((file) => {
+        const filePath = path.join(dirPath, file);
+        const stat = fs.statSync(filePath);
+        if (stat.isDirectory()) {
+            getEnvironment(filePath, filesTS);
+        } else if (filePath.endsWith('environment.ts')) {
+            filesTS.push(filePath);
+        }
+    });
+
+    return filesTS;
+}
+
+export function readEnvironment(filePathEnv){
+    const fileContent = fs.readFileSync(filePathEnv[0], 'utf8');
+    const matchResult = fileContent.match(/keyOfuscate: '(.*)'/) || fileContent.match(/keyOfuscate: "(.*)"/);
+    return matchResult ? matchResult[1] : null;
+}
+
+
+export function encryptQuery(filePath,secretKey) {
+    let constList = '';
+
+    let fileContent = fs.readFileSync(filePath, 'utf8');
+    fileContent = fileContent.replace(/import {gql} from "graphql-tag";\n/, '');
+
+    const constants = [];
+
+    const regex = /export const (\w+)\s*=\s*([^;]+);/g;
+
+    let match;
+    while (match = regex.exec(fileContent)) {
+        const constantName = match[1];
+        const constantValue = match[2].replace('gql', '').replace('`', '').replace('`', '');
+        constants.push({
+            nameConst: constantName,
+            valueConst: constantValue
+        });
+    }
+
+    constants.forEach(data => {
+        const encryptedQuery = CryptoJS.AES.encrypt(data.valueConst, secretKey).toString();
+        constList = constList + `export const ${data.nameConst} = \`${encryptedQuery}\`` + '\n';
+    });
+    fs.writeFileSync(filePath, constList);
+    console.log('--------------------------------');
+    console.log('Encrypt Complete path: ' + filePath);
+    console.log('--------------------------------');
+}
+
+export function getFilesWithQueryTs(dirPath, files = []) {
+    const fileList = fs.readdirSync(dirPath);
+
+    fileList.forEach(file => {
+        const filePath = path.join(dirPath, file);
+        const stat = fs.statSync(filePath);
+
+        if (stat.isDirectory()) {
+            getFilesWithQueryTs(filePath, files);
+        } else if (filePath.endsWith('.query.ts')) {
+            files.push(filePath);
+        }
+    });
+
+    return files;
+}
+
+
